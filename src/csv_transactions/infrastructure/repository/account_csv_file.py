a9c83708edafff0a48e389b7b0c16332556d269b
jonathan.neyra-at-416851658544 <jneyra.upax@gmail.com>
2022-12-30 00:33:45 -0600

    [FIX] - CSV Path

--- src/csv_transactions/infrastructure/repository/account_csv_file.py
+++ src/csv_transactions/infrastructure/repository/account_csv_file.py
@@ -1,10 +1,14 @@
 import csv
+import os.path
+
 from ...model.contracts import CsvFileRepository
 
 
 class AccountCsvFileRepository(CsvFileRepository):
-    path_origin: str = "transactions/"
     input_file_extension: str = "csv"
+
+    def __init__(self):
+        self.path_origin = f"{os.path.dirname(__file__)}/../../data/"
 
     def account_file(self, account_name: str):
         account_path = "{origin}{account}.{ext}".format(
@@ -12,6 +16,9 @@ class AccountCsvFileRepository(CsvFileRepository):
             account=account_name,
             ext=self.input_file_extension
         )
+
+        print("Path: ", account_path)
+
         csv_file = open(account_path, 'r')
         account_csv = csv.reader(csv_file, delimiter=',')
 
